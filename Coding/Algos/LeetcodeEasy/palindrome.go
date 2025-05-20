388ba7b92b644d85871fbcc65089a07c5d06f303
Ivan Sizov <127422441+Frozelo@users.noreply.github.com>
2025-05-20 23:34:00 +0700

    Create palindrome.go

---
+++ Coding/Algos/LeetcodeEasy/palindrome.go
@@ -0,0 +1,21 @@
+// Given an integer x, return true if x is a palindrome , and false otherwise.
+// example 121 is palindrome, 1 2 1 -> -121 is not a palindrome
+
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 125
+	var res int
+	for a > 0 {
+		prem := a % 10
+		res = res*10 + prem
+		a = a / 10
+
+		fmt.Println(res)
+	}
+}
