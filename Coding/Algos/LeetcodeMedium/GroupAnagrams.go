21e8df932c9c313faa6736eba2cc592db579f6f0
Ivan Sizov <127422441+Frozelo@users.noreply.github.com>
2025-05-25 01:58:48 +0700

    Create GroupAnagrams.go

---
+++ Coding/Algos/LeetcodeMedium/GroupAnagrams.go
@@ -0,0 +1,8 @@
+// Given an array of strings strs, group the annagrams together. You can return the answer in any order.
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// https://leetcode.com/problems/group-anagrams/description/
+
+// WIP
