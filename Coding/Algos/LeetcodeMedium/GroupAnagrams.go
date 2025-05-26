eb20719b4b096d351712190c2045498fbc5a5634
Ivan Sizov <127422441+Frozelo@users.noreply.github.com>
2025-05-27 02:21:53 +0700

    Update GroupAnagrams.go

--- Coding/Algos/LeetcodeMedium/GroupAnagrams.go
+++ Coding/Algos/LeetcodeMedium/GroupAnagrams.go
@@ -5,4 +5,30 @@
 
 // https://leetcode.com/problems/group-anagrams/description/
 
-// WIP
+
+// O(n*log(n) *k )
+func groupAnagrams(strs []string) [][]string {
+    m := make(map[string][]string)
+
+    for _, str := range strs {
+        runes := []rune(str)
+
+        sort.Slice(runes, func(i, j int) bool {
+            return runes[i] < runes[j]
+        })
+
+
+        sortedStr := string(runes)
+
+        m[sortedStr] = append(m[sortedStr], str)
+    }
+
+    res := make([][]string, 0, len(m))
+
+
+    for _, words := range m {
+        res = append(res, words)
+    }
+
+    return res
+}
