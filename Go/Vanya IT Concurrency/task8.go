1f33ebcc7a755dec44ed42a18a1baa7f46e49010
Ivan Sizov <127422441+Frozelo@users.noreply.github.com>
2025-09-01 18:58:33 +0700

    Create task8.go

---
+++ Go/Vanya IT Concurrency/task8.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// TASK 8
+// Написать функцию обертку для executeTask, которая:
+// исполняет executeTask
+// принимает аргументом контекст
+// завершается либо в результате исполнения, либо в результате отмены контекста. В последнем случае вернуть ошибку контекста
+
+const timeout = 100 * time.Millisecond
+
+func executeTask() {
+	time.Sleep(time.Duration(rand.Intn(3)) * timeout)
+}
+
+func executeTaskWithTimeout(ctx context.Context) error {
+	// сделать что-то чтобы понять, что все ок и executeTask выполнилась
+	// <- struct{}
+
+	ch := make(chan struct{})
+	defer close(ch)
+	go func() {
+		executeTask()
+		ch <- struct{}{}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-ch:
+		return nil
+	}
+}
+
+func main() {
+	ctx, _ := context.WithTimeout(context.Background(), timeout)
+	err := executeTaskWithTimeout(ctx)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("task done")
+}
